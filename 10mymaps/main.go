b99757b6d7dd604d8f5ad231a0a6788a40a35f00
dharmeeshsingh01 <dharmeshsingh@live.com>
2022-09-22 21:18:37 +0530

    maps in golang with loops

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -10,6 +10,23 @@ func main() {
 	language["JS"] = "javascript"
 	language["RE"] = "Reactjs"
 	language["PY"] = "Python"
+	language["RB"] = "Ruby"
 	fmt.Println("List of All Language", language)
 	fmt.Println("Js shorts for", language["JS"])
+
+	fmt.Println("Deleteing value from map ")
+	// Deleteing
+	delete(language, "RB")
+	fmt.Println("List of All Language after delete", language)
+
+	// loops are intresing in Golang
+
+	for key, value := range language {
+		fmt.Printf("For key %v, value is %v\n", key, value)
+	}
+
+	// other syntax is comma err,
+	for _, val := range language {
+		fmt.Printf("Value is %v\n", val)
+	}
 }
